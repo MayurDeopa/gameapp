66b80179853a25bd4ee565c49d426f4bd7eb7be1
MayurDeopa <frostkickerzvashi@gmail.com>
2022-02-09 22:24:44 +0530

    removed unused component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import { useState } from 'react';
 import './App.css';
 import Login from './Components/Login/Login';
 import SideBar from './Components/SideBar/SideBar';
 
 function App() {
 
-  const [toggle,setToggle]= useState(false)
   return (
     <div className="App">
-      <SideBar value={toggle}/>
+      <SideBar/>
       <Login/>
     </div>
   );
